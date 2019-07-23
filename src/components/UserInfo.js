160e5d00a6655905c552cb7a16987d3e7eaa7d91
Polina Lappo <polina.lappo@soft-werke.com>
2019-07-24 02:31:38 +0300

    full user info

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,11 +1,37 @@
 import React, { Component} from 'react';
+import UserCard from './UserCard';
+import { connect } from 'react-redux';
+import { usersFetchData } from '../actions/users';
 
 class UserInfo extends Component {
 
+    componentDidMount() {
+        if(!this.props.users.length) {
+            this.props.fetchData('http://78qj8.mocklab.io/users');
+        }
+    }
+    
     render(){
-    return <div className='user-info'>
-        <p>this is user page</p>
-    </div>}
+        const { userId } = this.props;
+        const user = this.props.users.find((i) => i.id == userId)
+
+        return <div className='user-info'>
+            <UserCard fullInfo={true} {...user}/>
+        </div>}
 }
 
-export default UserInfo
+const mapStateToProps = (state) => {
+    return {
+        users: state.users,
+        haveErrored: state.usersHaveErrored,
+        areLoading: state.usersAreLoading
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchData: (url) => dispatch(usersFetchData(url))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserInfo);
