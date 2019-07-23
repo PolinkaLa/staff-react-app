c0189d9a9af480dcc13b428aad60e99463b3a045
Polina Lappo <polina.lappo@soft-werke.com>
2019-07-24 02:31:59 +0300

    commenr list component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Comment from './Comment';
+import { connect } from 'react-redux';
+import { commentsFetchData } from '../actions/comments';
 
-function CommentList(){
-    return <div className='comment-list'>
-        this is comments
-    </div>
+class CommentList extends Component {
+
+    componentDidMount() {
+        this.props.fetchData(`http://78qj8.mocklab.io/comments/${this.props.userId}`);
+    }
+
+    render() {
+        if (this.props.haveErrored) {
+            return <p>Error...</p>;
+        }
+
+        if (this.props.areLoading) {
+            return <p>Loading…</p>;
+        }
+
+        return (
+            <div className='comment-list'>
+                {this.props.comments.map((comment) => (
+                        <Comment key={comment.id} {...comment}/>
+                ))}
+            </div>
+        )
+    }
 }
 
-export default CommentList;
+const mapStateToProps = (state) => {
+    return {
+        comments: state.comments,
+        haveErrored: state.commentsHaveErrored,
+        areLoading: state.commentsAreLoading
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchData: (url) => dispatch(commentsFetchData(url))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentList);
