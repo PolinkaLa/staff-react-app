a6be74ee8a276f8a935602c19ca3882553754256
plappo <polina.lappo@soft-werke.com>
2019-07-23 18:28:31 +0300

    add components to detail page

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import UserInfo from '../components/UserInfo';
+import UserCarusel from '../components/UserCarusel';
+import { browserHistory } from 'react-router';
+import CommentList from '../components/CommentList';
+import CommentForm from '../components/CommentForm';
 
 function Detail(props) {
     return <div>
+        <button onClick={browserHistory.goBack}>Go Back</button>
+        <UserCarusel/>
         <UserInfo userId={props.params.userId}/>
+        <CommentList/>
+        <CommentForm/>
     </div>
 }
 
